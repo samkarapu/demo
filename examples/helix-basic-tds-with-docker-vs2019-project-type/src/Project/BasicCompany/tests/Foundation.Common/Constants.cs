2a8ff3ad3614c3c76e1e5d3d68e3ad6da5c40ffc
Göran Halvarsson <goranhalvarsson@gmail.com>
2020-04-16 10:34:16 +0200

    Adding InternetExplorerDriver

--- examples/helix-basic-tds-with-docker-vs2019-project-type/src/Project/BasicCompany/tests/Foundation.Common/Constants.cs
+++ examples/helix-basic-tds-with-docker-vs2019-project-type/src/Project/BasicCompany/tests/Foundation.Common/Constants.cs
@@ -8,6 +8,7 @@
       public const string Browser = "Browser";
       public const string EdgeWebDriver = "EdgeWebDriver";
       public const string EdgeBinaryLocation = "EdgeBinaryLocation";
+      public const string InternetExporerVersion = "InternetExporerVersion";
     }
   }
 }
--- examples/helix-basic-tds-with-docker-vs2019-project-type/src/Project/BasicCompany/tests/Foundation.Common/Extensions/SeleniumExtensions.cs
+++ examples/helix-basic-tds-with-docker-vs2019-project-type/src/Project/BasicCompany/tests/Foundation.Common/Extensions/SeleniumExtensions.cs
@@ -1,8 +1,8 @@
 ﻿using BasicCompany.Foundation.Common.UITests.Infrastructure;
+using Microsoft.Edge.SeleniumTools;
 using Microsoft.Extensions.Configuration;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
-using OpenQA.Selenium.Edge;
 using OpenQA.Selenium.Firefox;
 using OpenQA.Selenium.IE;
 using OpenQA.Selenium.Safari;
@@ -36,7 +36,6 @@ namespace BasicCompany.Foundation.Common.UITests.Extensions
       wait.Until<IWebElement>((d) => d.FindElement(selector));
 
       return driver.FindElement(selector);
-
     }
 
 
@@ -48,26 +47,16 @@ namespace BasicCompany.Foundation.Common.UITests.Extensions
       {
 
         case BrowserTypes.Edge:
-          var options = new Microsoft.Edge.SeleniumTools.EdgeOptions
+          EdgeOptions egdeOptions = new EdgeOptions
           {
             PageLoadStrategy = PageLoadStrategy.Normal,
             UseChromium = true,
             BinaryLocation = configuration[Constants.EnvironmentVariableKeys.EdgeBinaryLocation],
           };
 
-          //var options = new Microsoft.Edge.SeleniumTools.EdgeOptions
-          //{
-          //  PageLoadStrategy = PageLoadStrategy.Normal,
+          EdgeDriverService edgeDriverService = EdgeDriverService.CreateChromiumService(configuration[Constants.EnvironmentVariableKeys.EdgeWebDriver]);
 
-          //};
-
-          var service = Microsoft.Edge.SeleniumTools.EdgeDriverService.CreateChromiumService(configuration[Constants.EnvironmentVariableKeys.EdgeWebDriver]);
-
-          //System.setProperty("webdriver.edge.driver", "C:\\Program Files (x86)\\Microsoft Web Driver\\MicrosoftWebDriver.exe"); //put actual location
-
-          //specFlowContext.Set((IWebDriver)new Microsoft.Edge.SeleniumTools.EdgeDriver(@"C:\Slask\sss", options));
-
-          specFlowContext.Set((IWebDriver)new Microsoft.Edge.SeleniumTools.EdgeDriver(service, options));
+          specFlowContext.Set((IWebDriver)new EdgeDriver(edgeDriverService, egdeOptions));
           break;
 
         case BrowserTypes.Firefox:
@@ -75,7 +64,14 @@ namespace BasicCompany.Foundation.Common.UITests.Extensions
           break;
 
         case BrowserTypes.InternetExplorer:
-          specFlowContext.Set((IWebDriver)new InternetExplorerDriver());
+
+          InternetExplorerOptions internetExplorerOptions = new InternetExplorerOptions()
+          {
+            BrowserVersion = configuration[Constants.EnvironmentVariableKeys.InternetExporerVersion],
+            IgnoreZoomLevel = true
+          }; 
+
+          specFlowContext.Set((IWebDriver)new InternetExplorerDriver(internetExplorerOptions));
           break;
 
         case BrowserTypes.Safari:
@@ -85,14 +81,12 @@ namespace BasicCompany.Foundation.Common.UITests.Extensions
           specFlowContext.Set((IWebDriver)new SafariDriver(safariDriverService));
           break;
 
-
         default:
           {
             ChromeDriverService chromeDriverService = ChromeDriverService.CreateDefaultService();
             chromeDriverService.SuppressInitialDiagnosticInformation = true;
             specFlowContext.Set((IWebDriver)new ChromeDriver(chromeDriverService));
           }
-
           break;
       }
 
